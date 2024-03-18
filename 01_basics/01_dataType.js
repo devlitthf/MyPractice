67a895c4da85e91913a1ed81f28985b03bd49c2b
devlitthf <161701953+devlitthf@users.noreply.github.com>
2024-03-18 06:47:49 +0000

    dataType file added

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,14 @@
+const naam = "Devraj"
+let daata = 123
+var alag = 444
+different = "Location"
+let location;
+
+
+// naam = "Raikwar" Not allowed because of 
+// console.log(naam)
+
+/**
+ * Prefer not to use [var] because of issue in block scope and functional scope
+ */
+console.table([naam, daata, alag, different, location])
