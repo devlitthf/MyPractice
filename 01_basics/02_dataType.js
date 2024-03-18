6f14677d1fa577c06c6c8618fc8e41e3501fbede
devlitthf <161701953+devlitthf@users.noreply.github.com>
2024-03-18 07:07:47 +0000

    01_variable.js file name changed from dataType and new file created as 02_dataType.js

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; // treal all JS code as newer version
+//alert (3 + 3) // we are using nodejs, not browser
+// https://ecma-international.org/publications-and-standards/standards/ecma-262/
+
+console.log("Devraj");
+
+let name = "dev"
+let age = 10
+let isLogin = false
+
+/**
+ * number => 2 to power 53
+ * biging
+ * string => ""
+ * boolean => true/false
+ * null => standalone value
+ * undefined =>
+ * symbol => unique
+ * 
+ * object
+ */
+
+console.log(typeof undefined); // Output = undefined
+console.log(typeof null); // Output = object
