00d15994eb0d7a5521429c0b4d3a2166082a7145
devlitthf <161701953+devlitthf@users.noreply.github.com>
2024-03-20 09:55:30 +0000

    conversion of varibale in investigation type done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,23 @@
+// let score = "33abc"
+// let score = undefined
+// let score = true
+// let score = null
+// let isLoggedIn = "devraj"
+// let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+/**
+ * This 1 one example will return "NaN= number" type variable if we convert it. and check the type
+ * This 2 one example will return "NaN" type variable when we check
+ * This 3 one example will return "1,0 (depends on true or false)" type variable when we check
+ * This 4 one example will return "0" type variable when we check 
+ * This 5 one example will return "true" type variable in boolean conversion 
+ * This 6 one example will return "false" type variable in boolean conversion 
+ */
+
 let score = 33
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
