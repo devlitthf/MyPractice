7ab33050b1dc138d1e2c3e8126e3c82255695d45
devlitthf <161701953+devlitthf@users.noreply.github.com>
2024-03-26 05:23:46 +0000

    Heap, Stack Memory

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -8,10 +8,10 @@
 
 // Array, Objects, Function
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 23234252135253151235n // to make a value bigInt we associate 'n' numbers last
 
@@ -19,15 +19,37 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 // typeof value = object
-let myObj = {
-     name: "hitesh",
-     age: 22,
-}
+// let myObj = {
+    //  name: "hitesh",
+    //  age: 22,
+// }
 // typeof value is function but we call all the referance (Non primitive) data type object function
 
-const myFunction =function(){
-    console.log("Hello world");
-}
+// const myFunction =function(){
+    // console.log("Hello world");
+// }
 // typeof value is function);
+
+// ******************************************************
+
+let myYoutubeName = "devrajraikwar.com"
+
+let anotherName = myYoutubeName
+anotherName = "hitropphy"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "devraj@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
