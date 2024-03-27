2bb7892557bf2add653309dbf30ad63d798dd8c5
devlitthf <161701953+devlitthf@users.noreply.github.com>
2024-03-27 04:37:07 +0000

    num and math overview is completed

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -17,8 +17,19 @@
 
 // ****************** Maths ********************************
 
-console.log(Math);
-console.log(Math.abs(-234))
-console.log(Math.round(43.50));
-console.log(Math.ceil(4.3));
-console.log(Math.floor(4.5));
+// console.log(typeof Math);
+// console.log(`This is the absolute value of Integer number only ${Math.abs(-234.89)}`)
+// console.log(`This is the round figure value by round method ${Math.round(43.50)}`);
+// console.log(`This is Ceil Method Average value in Increased scale ${Math.ceil(4.3)}`);
+// console.log(`This is Floor Method Average value in Decreased scale ${Math.floor(4.5)}`);
+// console.log(`This is the minimum value in array form ${Math.min(4,3,5,10)}`);
+// console.log(`This is the maximus value in array form ${Math.max(4,3,5,10)}`);
+
+console.log(`${Math.random()}`);
+console.log(`${(Math.random()*10) + 1}`);
+console.log(`${Math.floor(Math.random()*10) + 1}`);
+
+const min = 10
+const max = 20
+
+console.log(`${Math.floor(Math.random() * (max - min + 1)) + min}`)
